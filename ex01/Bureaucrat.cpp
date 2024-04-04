0c13f491415bc831707aa5250882ede5fff4763a
Seonggon, Cho <77792994+jmsmg@users.noreply.github.com>
2024-04-04 06:49:10 +0000

    cp ex00 -> ex01

---
+++ ex01/Bureaucrat.cpp
@@ -0,0 +1,93 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(): _name("default"), _grade(150)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade): _name(name)
+{
+	if (grade < 1)
+	{
+		throw Bureaucrat::GradeTooHighException();
+	}
+	else if (150 < grade)
+	{
+		throw Bureaucrat::GradeTooLowException();
+	}
+	this->_grade = grade;
+	std::cout << "constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &bureaucrat)
+{
+	this->_name = bureaucrat._name;
+	this->_grade = bureaucrat._grade;
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+}
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &bureaucrat)
+{
+	if (this != &bureaucrat)
+	{
+		this->_name = bureaucrat._name;
+		this->_grade = bureaucrat._grade;
+		std::cout << "Bureaucrat copy assignment operator called" << std::endl;
+	}
+	return (*this);
+
+}
+
+Bureaucrat::~Bureaucrat(){}
+
+int			Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+std::string	Bureaucrat::getName(void) const
+{
+	return (this->_name);
+}
+
+void	Bureaucrat::increaseGrade()
+{
+	std::cout << "Bureaucrat increaseGrade function called" << std::endl;
+	if (this->_grade == 1)
+	{
+		throw GradeTooHighException();
+	}
+	else
+	{
+		this->_grade--;
+		std::cout << "Bruaucrat grade : " << this->_grade + 1 << " -> " << this->_grade << std::endl;
+	}
+}
+
+void	Bureaucrat::decreaseGrade()
+{
+	if (this->_grade == 150)
+	{
+		throw GradeTooLowException();
+	}
+	else
+	{
+		this->_grade++;
+		std::cout << "Bruaucrat grade : " << this->_grade - 1 << " -> " << this->_grade << std::endl;
+	}
+}
+
+const	char *Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+	return ("Error : Grade too high");
+}
+
+const	char *Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+	return ("Error : Grade too Low");
+}
+
+std::ostream	&operator<<(std::ostream &out, const Bureaucrat &obj)
+{
+	out << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+	return (out);
+}
---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class	Bureaucrat
+{
+	private:
+		std::string	_name;
+		int			_grade;
+
+		class	GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what(void) const throw();
+		};
+
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what(void) const throw();
+		};
+
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &clone);
+		Bureaucrat	&operator=(const Bureaucrat &bureaucrat);
+		~Bureaucrat();
+
+		std::string	getName() const;
+		int			getGrade() const;
+		void		increaseGrade();
+		void		decreaseGrade();
+
+};
+
+std::ostream	&operator<<(std::ostream &out, const Bureaucrat &obj);
+#endif
